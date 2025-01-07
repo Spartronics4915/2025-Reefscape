96b545209e7caf9a69ef30293f8322975ce5cf30
sharkey300 <84826384+sharkey300@users.noreply.github.com>
2025-01-07 11:32:02 -0800

    Add method to get a vision measurement from a limelight using MegaTag2

--- src/main/java/com/spartronics4915/frc2025/Constants.java
+++ src/main/java/com/spartronics4915/frc2025/Constants.java
@@ -45,4 +45,8 @@ public final class Constants {
     public static final class DriveCommandConstants {
         public static final PIDConstants kAnglePIDConstants = new PIDConstants(16, 0, 0);
     }
+
+    public static final class VisionConstants {
+        public static final double kMaxAngularSpeed = 720;
+    }
 }
--- src/main/java/com/spartronics4915/frc2025/subsystems/vision/LimelightDevice.java
+++ src/main/java/com/spartronics4915/frc2025/subsystems/vision/LimelightDevice.java
@@ -1,10 +1,19 @@
 package com.spartronics4915.frc2025.subsystems.vision;
 
-import com.spartronics4915.frc2025.LimelightHelpers;
+import java.util.Optional;
 
+import com.spartronics4915.frc2025.LimelightHelpers;
+import com.spartronics4915.frc2025.Constants.VisionConstants;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import swervelib.SwerveDrive;
 
 public class LimelightDevice extends SubsystemBase {
+
+    public static record VisionMeasurement(Pose2d pose, double timestamp) {}
+
     private final String name;
 
     public LimelightDevice(String name) {
@@ -21,5 +30,23 @@ public class LimelightDevice extends SubsystemBase {
 
     public boolean getTv() {
         return LimelightHelpers.getTV(name);
+    }
+
+    public Optional<VisionMeasurement> getVisionMeasurement(SwerveDrive swerve) {
+        boolean rejectUpdate = false;
+        LimelightHelpers.SetRobotOrientation(name, swerve.getOdometryHeading().getDegrees(), 0, 0, 0, 0, 0);
+        LimelightHelpers.PoseEstimate megaTag2 = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(name);
+        if (swerve.getGyro().getYawAngularVelocity().abs(Units.DegreesPerSecond) > VisionConstants.kMaxAngularSpeed) {
+            rejectUpdate = true;
+        }
+        if (megaTag2.tagCount == 0) {
+            rejectUpdate = true;
+        }
+        if (rejectUpdate) {
+            return Optional.empty();
+        } else {
+            return Optional.of(new VisionMeasurement(megaTag2.pose, megaTag2.timestampSeconds));
+        }
+
     }
 }
