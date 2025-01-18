e758d0dd1f3d79ec568b576bdc794cadf47275cc
Daniil Ivanov <daniil.here@gmail.com>
2025-01-17 17:09:36 -0800

    Set up elevator framework

---
+++ src/main/java/com/spartronics4915/frc2025/subsystems/coral/ElevatorSubsystem.java
@@ -0,0 +1,19 @@
+package com.spartronics4915.frc2025.subsystems.coral;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ElevatorSubsystem extends SubsystemBase {
+    
+    public ElevatorSubsystem() {
+        // Initialize the 2 Neo motors and home sensor here
+    }
+    public void moveToDock() {
+        
+    }
+    public void moveToL3() {
+        
+    }
+    public void moveToL4() {
+        
+    }
+}
