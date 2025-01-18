e5dd97f546ef56820bd2ccf926cd47ad5eda29b2
Gage <lockettgage14@gmail.com>
2025-01-18 13:40:45 -0800

    add motor config settings

--- src/main/java/com/spartronics4915/frc2025/subsystems/ClimberSubsystem.java
+++ src/main/java/com/spartronics4915/frc2025/subsystems/ClimberSubsystem.java
@@ -2,16 +2,20 @@ package com.spartronics4915.frc2025.subsystems;
 
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimberSubsystem extends SubsystemBase {
 
-    private SparkMax motor1;
-    private SparkMax motor2;
+    //private SparkMax motorLeft;
+    private SparkMax motorRight;
+
     
-    //boolean isInverted = motor2.configAccessor.getInverted();
+    
+    
 
     public enum ClimberState {
         CAGE(Rotation2d.fromDegrees(0), 7),
@@ -22,8 +26,8 @@ public class ClimberSubsystem extends SubsystemBase {
         public final double claw;
 
         private ClimberState(Rotation2d angle, double claw) {
-                    this.angle = angle;
-                    this.claw = claw;
+            this.angle = angle;
+            this.claw = claw;
         }
 
         public Rotation2d getAngle() {
@@ -34,11 +38,26 @@ public class ClimberSubsystem extends SubsystemBase {
     }
     
     public ClimberSubsystem () {                 //-need fill in motor Ids
-        motor1 = new SparkMax(0, MotorType.kBrushless);
-        motor2 = new SparkMax(0, MotorType.kBrushless);
+        /*
+        SparkMaxConfig Left = new SparkMaxConfig();
+        Left.idleMode(IdleMode.kBrake);
+        Left.inverted(true);
+        motorLeft = new SparkMax(1, MotorType.kBrushless);
+        motorLeft.configure(Left, null, null);
+        */
+
+
+        SparkMaxConfig Right = new SparkMaxConfig();
+        Right.idleMode(IdleMode.kBrake);
+        motorRight = new SparkMax(0, MotorType.kBrushless);
+        motorRight.configure(Right, null, null);
+
+
     }
 
     public void grab () {
+        SparkMaxConfig Right = new SparkMaxConfig();
+        Right.idleMode(IdleMode.kBrake);
 
     }
 
