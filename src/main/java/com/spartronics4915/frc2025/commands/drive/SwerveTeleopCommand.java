f6745ffd7785a99f69cd15561463ce8f4122dbe4
otter502 <115671877+otter502@users.noreply.github.com>
2025-01-27 10:28:45 -0800

    Remove unused imports in swerve teleop command

--- src/main/java/com/spartronics4915/frc2025/commands/drive/SwerveTeleopCommand.java
+++ src/main/java/com/spartronics4915/frc2025/commands/drive/SwerveTeleopCommand.java
@@ -1,24 +1,13 @@
 package com.spartronics4915.frc2025.commands.drive;
 
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StructPublisher;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.RobotBase;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
-import static com.spartronics4915.frc2025.commands.drive.ChassisSpeedSuppliers.computeVelocitiesFromController;
-import static com.spartronics4915.frc2025.commands.drive.ChassisSpeedSuppliers.getAngleJoystickAngle;
 import static com.spartronics4915.frc2025.commands.drive.ChassisSpeedSuppliers.getSwerveTeleopCSSupplier;
-import static com.spartronics4915.frc2025.commands.drive.ChassisSpeedSuppliers.gotoAngle;
-
-import com.spartronics4915.frc2025.Constants.Drive;
-import com.spartronics4915.frc2025.Constants.OI;
 import com.spartronics4915.frc2025.subsystems.SwerveSubsystem;
 
 public class SwerveTeleopCommand extends Command {
