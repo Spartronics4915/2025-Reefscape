e3c111d7683c42a15f6b27dd7d2cf7f4973eba90
Finn <148401810+FinnSpartronics@users.noreply.github.com>
2025-02-01 13:36:34 -0800

    (ominous) Bling has returned.

---
+++ src/main/java/com/spartronics4915/frc2025/subsystems/bling/BlingSegment.java
@@ -0,0 +1,42 @@
+package com.spartronics4915.frc2025.subsystems.bling;
+
+import edu.wpi.first.wpilibj.AddressableLEDBufferView;
+import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj.util.Color;
+
+public abstract class BlingSegment {
+    protected int frame = 0;
+    protected int ledLength;
+    protected int maxLength = -1;
+    AddressableLEDBufferView buffer;
+
+    final public int getStripLength() {
+        return this.ledLength;
+    }
+
+    final private void incrementFrame() {
+        frame++;
+        if (maxLength > -1)
+            if (frame > maxLength) frame = 0;
+    }
+
+    final public void update() {
+        updateLights();
+        incrementFrame();
+    }
+    abstract protected void updateLights();
+
+    public static final BlingShow show(String filename) {
+        return new BlingShow(filename);
+    }
+    public static final BlingLEDPattern solid(int r, int g, int b, int length) {
+        return new BlingLEDPattern(LEDPattern.solid(new Color(r,g,b)), length);
+    }
+    public static final BlingLEDPattern solid(Color color, int length) {
+        return new BlingLEDPattern(LEDPattern.solid(color), length);
+    }
+    public static final BlingLEDPattern rainbow(int length) {
+        return new BlingLEDPattern(LEDPattern.rainbow(255,255), length);
+    }
+
+}
