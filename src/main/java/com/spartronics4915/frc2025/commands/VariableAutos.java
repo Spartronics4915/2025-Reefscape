6777df53249e1af978f70f3bf2fb8456449b2c25
otter502 <115671877+otter502@users.noreply.github.com>
2025-03-05 18:49:04 -0800

    Adjust offsets for static positions

--- src/main/java/com/spartronics4915/frc2025/commands/VariableAutos.java
+++ src/main/java/com/spartronics4915/frc2025/commands/VariableAutos.java
@@ -68,8 +68,8 @@ public class VariableAutos {
     }
 
     public enum BranchSide{
-        LEFT(new Translation2d(0.108759, 0.5152845)),
-        RIGHT(new Translation2d(0.218062, 0.5154565));
+        LEFT(new Translation2d(0.108759 + 0.0381 + 0.00635, 0.5152845 + 0.0254)),
+        RIGHT(new Translation2d(0.218062, 0.5154565 + 0.0254));
 
         public Translation2d tagOffset;
         private BranchSide(Translation2d offsets) {
