af70c8a53635a70e114d50a5aa9771b03df89eb8
sharkey300 <84826384+sharkey300@users.noreply.github.com>
2025-03-06 16:54:42 -0800

    Modify reef orient to point towards center and extend reef region into barge
    This also disables the barge alignment logic, but this is fine as drive team can align easily and we'd rather prevent the robot from quickly turning on the border between the reef and the barge

--- src/main/java/com/spartronics4915/frc2025/Constants.java
+++ src/main/java/com/spartronics4915/frc2025/Constants.java
@@ -288,6 +288,8 @@ public final class Constants {
 
     public static final class OrientTowardsNearestPOIConstants {
         public static final Rotation2d REEF_OFFSET = Rotation2d.k180deg;
+        public static final Translation2d REEF_CENTER_RED = new Translation2d(13.067, 4.031);
+        public static final Translation2d REEF_CENTER_BLUE = new Translation2d(4.471, 4.031);
         public static final double CORAL_STATION_ANGLE = 55;
         public static final Rotation2d BARGE_ROTATION = Rotation2d.kCCW_90deg;
         public static final Translation2d[] BARGE_RED_CAGE_POSITIONS = {
