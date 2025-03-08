827d08405692a9b6befaa8c132b5850bce6155f8
sharkey300 <84826384+sharkey300@users.noreply.github.com>
2025-03-08 12:39:02 -0800

    Set ll 13 to reef so it can be a backup

--- src/main/java/com/spartronics4915/frc2025/Constants.java
+++ src/main/java/com/spartronics4915/frc2025/Constants.java
@@ -350,7 +350,7 @@ public final class Constants {
         public static final LimelightConstants kLimelights[] = {
                 new LimelightConstants("alex", LimelightModel.LIMELIGHT_3G, 11, LimelightRole.REEF),
                 new LimelightConstants("randy", LimelightModel.LIMELIGHT_3, 12, LimelightRole.NOTHING),
-                new LimelightConstants("ben", LimelightModel.LIMELIGHT_3G, 13, LimelightRole.NOTHING),
+                new LimelightConstants("ben", LimelightModel.LIMELIGHT_3G, 13, LimelightRole.REEF), //TODO: CHANGE
                 new LimelightConstants("chucky", LimelightModel.LIMELIGHT_3, 14, LimelightRole.NOTHING),
                 new LimelightConstants("doug", LimelightModel.LIMELIGHT_3, 15, LimelightRole.NOTHING)
         };
