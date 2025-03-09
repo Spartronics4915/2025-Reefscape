4004236906162cca329c429f8b5eeda1ee9c7fb6
otter502 <115671877+otter502@users.noreply.github.com>
2025-03-08 16:40:59 -0800

    Increase ArmConstraints

--- src/main/java/com/spartronics4915/frc2025/Constants.java
+++ src/main/java/com/spartronics4915/frc2025/Constants.java
@@ -411,7 +411,7 @@ public final class Constants {
         
         public static final double kDt = 0.02;
 
-        public static final Constraints kConstraints = new Constraints(4.5, 3.5 / 2.0); //8.0, 10
+        public static final Constraints kConstraints = new Constraints(5.0, 4.0 / 2.0); //8.0, 10
         public static final int kPeriodMs = 0;
 
         public static final double kS = 0.0;
