ab109e5ee3e74db5aa1b45b8777689ab765bd316
sharkey300 <84826384+sharkey300@users.noreply.github.com>
2025-03-13 14:32:02 -0700

    Log git metadata to SmartDashboard so they can be seen in riologs

--- src/main/java/com/spartronics4915/frc2025/Robot.java
+++ src/main/java/com/spartronics4915/frc2025/Robot.java
@@ -48,9 +48,18 @@ public class Robot extends TimedRobot {
     }
 
     @Override
+    @SuppressWarnings("all")
     public void robotInit() {
         WebServer.start(5800, Filesystem.getDeployDirectory().getPath());
         FollowPathCommand.warmupCommand().schedule();
+
+        // BuildConstants will generate when you build
+        SmartDashboard.putString("Git/SHA", BuildConstants.GIT_SHA);
+        SmartDashboard.putString("Git/Branch", BuildConstants.GIT_BRANCH);
+        SmartDashboard.putString("Git/Commit Date", BuildConstants.GIT_DATE);
+        SmartDashboard.putString("Git/Build Date", BuildConstants.BUILD_DATE);
+        SmartDashboard.putBoolean("Git/Dirty", BuildConstants.DIRTY == 1);
+        SmartDashboard.putNumber("Git/Revision", BuildConstants.GIT_REVISION);
     }
 
     /**
