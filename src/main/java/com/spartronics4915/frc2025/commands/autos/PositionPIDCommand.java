777f9594bb1c84a95c2740c2c79f3882e5795682
otter502 <115671877+otter502@users.noreply.github.com>
2025-03-18 11:33:37 -0700

    Remove trigger instance from position pid, instead using filter

--- src/main/java/com/spartronics4915/frc2025/commands/autos/PositionPIDCommand.java
+++ src/main/java/com/spartronics4915/frc2025/commands/autos/PositionPIDCommand.java
@@ -17,6 +17,7 @@ import com.spartronics4915.frc2025.Constants.Drive;
 import com.spartronics4915.frc2025.subsystems.SwerveSubsystem;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.networktables.BooleanPublisher;
@@ -25,6 +26,7 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.units.measure.Time;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class PositionPIDCommand extends Command{
@@ -33,12 +35,10 @@ public class PositionPIDCommand extends Command{
     public final Pose2d goalPose;
     private PPHolonomicDriveController mDriveController = Drive.AutoConstants.kAutoAlignPIDController;
 
-    private final Trigger endTrigger;
-    private final Trigger endTriggerDebounced;
-
     private final Timer timer = new Timer();
 
-    private final BooleanPublisher endTriggerLogger = NetworkTableInstance.getDefault().getTable("logging").getBooleanTopic("PositionPIDEndTrigger").publish();
+    private final Debouncer endTriggerDebouncer = new Debouncer(kEndTriggerDebounce.in(Seconds));
+
     private final DoublePublisher xErrLogger = NetworkTableInstance.getDefault().getTable("logging").getDoubleTopic("X Error").publish();
     private final DoublePublisher yErrLogger = NetworkTableInstance.getDefault().getTable("logging").getDoubleTopic("Y Error").publish();
 
@@ -47,28 +47,6 @@ public class PositionPIDCommand extends Command{
     private PositionPIDCommand(SwerveSubsystem mSwerve, Pose2d goalPose) {
         this.mSwerve = mSwerve;
         this.goalPose = goalPose;
-
-        endTrigger = new Trigger(() -> {
-            Pose2d diff = mSwerve.getPose().relativeTo(goalPose);
-
-            var rotation = MathUtil.isNear(
-                0.0, 
-                diff.getRotation().getRotations(), 
-                kRotationTolerance.getRotations(), 
-                0.0, 
-                1.0
-            );
-
-            var position = diff.getTranslation().getNorm() < kPositionTolerance.in(Meters);
-
-            var speed = mSwerve.getSpeed() < kSpeedTolerance.in(MetersPerSecond);
-
-            // System.out.println("end trigger conditions R: "+ rotation + "\tP: " + position + "\tS: " + speed);
-            
-            return rotation && position && speed;
-        });
-
-        endTriggerDebounced = endTrigger.debounce(kEndTriggerDebounce.in(Seconds));
     }
 
     public static Command generateCommand(SwerveSubsystem swerve, Pose2d goalPose, Time timeout){
@@ -80,7 +58,6 @@ public class PositionPIDCommand extends Command{
 
     @Override
     public void initialize() {
-        endTriggerLogger.accept(endTrigger.getAsBoolean());
         timer.restart();
     }
 
@@ -88,8 +65,6 @@ public class PositionPIDCommand extends Command{
     public void execute() {
         PathPlannerTrajectoryState goalState = new PathPlannerTrajectoryState();
         goalState.pose = goalPose;
-
-        endTriggerLogger.accept(endTrigger.getAsBoolean());
 
         mSwerve.drive(
             mDriveController.calculateRobotRelativeSpeeds(
@@ -103,7 +78,6 @@ public class PositionPIDCommand extends Command{
 
     @Override
     public void end(boolean interrupted) {
-        endTriggerLogger.accept(endTrigger.getAsBoolean());
         timer.stop();
 
         Pose2d diff = mSwerve.getPose().relativeTo(goalPose);
@@ -117,6 +91,25 @@ public class PositionPIDCommand extends Command{
 
     @Override
     public boolean isFinished() {
-        return endTriggerDebounced.getAsBoolean();
+
+        Pose2d diff = mSwerve.getPose().relativeTo(goalPose);
+
+        var rotation = MathUtil.isNear(
+            0.0, 
+            diff.getRotation().getRotations(), 
+            kRotationTolerance.getRotations(), 
+            0.0, 
+            1.0
+        );
+
+        var position = diff.getTranslation().getNorm() < kPositionTolerance.in(Meters);
+
+        var speed = mSwerve.getSpeed() < kSpeedTolerance.in(MetersPerSecond);
+
+        // System.out.println("end trigger conditions R: "+ rotation + "\tP: " + position + "\tS: " + speed);
+        
+        return endTriggerDebouncer.calculate(
+            rotation && position && speed
+        );
     }
 }
