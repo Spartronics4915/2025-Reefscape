e57cb3fa73d3002ba0b6b7332e35498027ae4f25
sharkey300 <84826384+sharkey300@users.noreply.github.com>
2025-03-19 14:52:11 -0700

    Intake when moving arm to remove algae

--- src/main/java/com/spartronics4915/frc2025/commands/DynamicsCommandFactory.java
+++ src/main/java/com/spartronics4915/frc2025/commands/DynamicsCommandFactory.java
@@ -426,6 +426,7 @@ public class DynamicsCommandFactory {
 
     public Command removeAlgaeArm() {
         return armSubsystem.setSetpointCommand(new Rotation2d(kRemoveAlgaeArmAngle))
+               .alongWith(intake())
                .onlyIf(() -> !isArmStowed());
     }
 
