1a43806a439c344e452da0e640980faab710dde9
otter502 <115671877+otter502@users.noreply.github.com>
2025-03-29 13:42:36 -0700

    Reduce intake lasercan trigger distance for intake

--- src/main/java/com/spartronics4915/frc2025/Constants.java
+++ src/main/java/com/spartronics4915/frc2025/Constants.java
@@ -73,7 +73,7 @@ public final class Constants {
         public static final int kLaserCANID = 21;
         public static final int kPipeLCID = 25;
 
-        public static final int laserCANDistance = 110;
+        public static final int laserCANDistance = 90;
         public static final int kBranchLCTriggerDist = 410;
         public static final double kBranchLCDebounceTime = 0.05;
 
