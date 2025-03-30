465befd25eada16d82f307d6705249c632db8169
otter502 <115671877+otter502@users.noreply.github.com>
2025-03-30 12:23:25 -0700

    Increase trigger distance

--- src/main/java/com/spartronics4915/frc2025/Constants.java
+++ src/main/java/com/spartronics4915/frc2025/Constants.java
@@ -280,7 +280,7 @@ public final class Constants {
 
             public static final PathConstraints kAutoPathConstraints = new PathConstraints(2.0, 2.0, 1/2 * Math.PI, 1 * Math.PI); //? consider making these more aggressive
             
-            public static final double kTriggerDistance = 2.5;
+            public static final double kTriggerDistance = 2.75;
 
             public static final double kAutoIntakeWaitTime = 0.1;
             public static final double kAutoIntakeTimeout = 0.2;
