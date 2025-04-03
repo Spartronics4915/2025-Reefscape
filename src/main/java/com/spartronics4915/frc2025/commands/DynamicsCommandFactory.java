ab2c4c8753730bb95c9c67d1a5ec202f2edd0b9b
otter502 <115671877+otter502@users.noreply.github.com>
2025-04-03 16:59:20 -0700

    Add logging for "can auto score"

--- src/main/java/com/spartronics4915/frc2025/commands/DynamicsCommandFactory.java
+++ src/main/java/com/spartronics4915/frc2025/commands/DynamicsCommandFactory.java
@@ -15,6 +15,7 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -183,8 +184,15 @@ public class DynamicsCommandFactory {
     }
 
     public boolean canAutoScore(){
-        return (elevatorSubsystem.getPosition() > DynaPreset.L4.getElevatorHeight() - Inches.of(10).in(Meters)) && 
-            isArmAtSetpoint(DynaPreset.L4.setpoint.armAngle, Degrees.of(10)) &&
+        boolean elev = elevatorSubsystem.getPosition() > DynaPreset.L4.getElevatorHeight() - Inches.of(10).in(Meters);
+        boolean arm = isArmAtSetpoint(DynaPreset.L4.setpoint.armAngle, Degrees.of(10));
+
+        SmartDashboard.putBoolean("Arm nearby setpoint", arm);
+        SmartDashboard.putBoolean("Elev nearby setpoint", elev);
+
+
+        return elev && 
+            arm &&
             intakeSubsystem.branchLC() && 
             isCoralInArm();
     }
