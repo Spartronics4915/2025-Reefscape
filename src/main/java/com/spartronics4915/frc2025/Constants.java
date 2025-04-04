f0540df6fc9f89379eb8c927b874fbf0e59a8778
otter502 <115671877+otter502@users.noreply.github.com>
2025-04-04 11:35:07 -0700

    Lowwer LC dist

--- src/main/java/com/spartronics4915/frc2025/Constants.java
+++ src/main/java/com/spartronics4915/frc2025/Constants.java
@@ -73,7 +73,7 @@ public final class Constants {
         public static final int kLaserCANID = 21;
         public static final int kPipeLCID = 25;
 
-        public static final int laserCANDistance = 90;
+        public static final int laserCANDistance = 50;
         public static final int kBranchLCTriggerDist = 450;
         public static final double kBranchLCDebounceTime = 0.01;
 
