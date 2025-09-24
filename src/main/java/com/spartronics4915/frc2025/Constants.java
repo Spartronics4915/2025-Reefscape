c8b1588dcb047a995c939149f73582248a751989
DanTCA <daniil.here@gmail.com>
2025-09-24 16:13:29 -0700

    Update intake speed for OUT state to improve performance
    There was a gear ratio change but changing it in code does nothing so I didn't change it

--- src/main/java/com/spartronics4915/frc2025/Constants.java
+++ src/main/java/com/spartronics4915/frc2025/Constants.java
@@ -105,7 +105,7 @@ public final class Constants {
         public enum IntakeSpeed {
             IN (-2500, -0.8),
             NEUTRAL (0, 0),
-            OUT (2500, 0.8),
+            OUT (2500, 1),
             FUNNEL_UNSTUCK(1800, 0.5);
 
             public final double intakeSpeed;
