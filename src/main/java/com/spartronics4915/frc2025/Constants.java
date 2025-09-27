42893482b19fc329fdbaa1738950582eccf9939c
DanTCA <daniil.here@gmail.com>
2025-09-27 14:26:48 -0700

    Tune algae score timing

--- src/main/java/com/spartronics4915/frc2025/Constants.java
+++ src/main/java/com/spartronics4915/frc2025/Constants.java
@@ -563,7 +563,7 @@ public final class Constants {
 
         public static final Time kJustScoredThreshold = Seconds.of(1);
 
-        public static final double algaeScoreDelay = 0.13;
+        public static final double algaeScoreDelay = 0.11;
 
         public static final int kFunnelLaserCanID = 20;
         public static final Distance funnelLCTriggerDist = Meters.of(0.2);
