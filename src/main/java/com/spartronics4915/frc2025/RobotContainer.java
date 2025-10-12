92862ebc1c56c33c24fa4f7d0f3b7efc8d0aa8b5
DanTCA <daniil.here@gmail.com>
2025-10-11 17:04:49 -0700

    Fix a bug where loadStow was called twice
    loadStow now is no longer called by hasScoredTrigger if in auto

--- src/main/java/com/spartronics4915/frc2025/RobotContainer.java
+++ src/main/java/com/spartronics4915/frc2025/RobotContainer.java
@@ -359,7 +359,7 @@ public class RobotContainer {
 
         //#region automated controls
 
-        dynamics.hasScoredTrigger.onTrue(dynamics.queueLoadStow());
+        dynamics.hasScoredTrigger.and(DriverStation::isTeleop).onTrue(dynamics.queueLoadStow());
 
         new Trigger(intakeSubsystem::detect).and(DriverStation::isTeleop)
             .debounce(0.02).onTrue(
