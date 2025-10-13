33e904a3fa3fdd0ad75cd2b8a6936c9e8046e6ec
DanTCA <daniil.here@gmail.com>
2025-10-12 22:27:48 -0700

    Reduce kScoreLaserCanDebounce to speed up auto

--- src/main/java/com/spartronics4915/frc2025/Constants.java
+++ src/main/java/com/spartronics4915/frc2025/Constants.java
@@ -558,7 +558,7 @@ public final class Constants {
         
         public static final double kElevatorSafeHeightSetpoint =  0.4;
 
-        public static final double kScoreLaserCanDebounce = 0.15; //seconds
+        public static final double kScoreLaserCanDebounce = 0.05; //seconds
 
         public static final double algaeIntakeDebounce = 0.5;
 
