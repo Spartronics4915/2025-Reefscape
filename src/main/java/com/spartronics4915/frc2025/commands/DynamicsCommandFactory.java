0579f527d1e7cc6e46142c2ab3dce09aa92e5fc6
DanTCA <daniil.here@gmail.com>
2025-10-18 08:56:56 -0700

    Change arm angle during climb

--- src/main/java/com/spartronics4915/frc2025/commands/DynamicsCommandFactory.java
+++ src/main/java/com/spartronics4915/frc2025/commands/DynamicsCommandFactory.java
@@ -126,7 +126,7 @@ public class DynamicsCommandFactory {
         L2(0.0, Rotation2d.fromDegrees(47.900)),
         L3(Meters.of(0.23939+0.1524-0.0254).in(Meters), Rotation2d.fromDegrees(58.10311200000001)),
         L4(Meters.of(1.23).in(Meters), Rotation2d.fromDegrees(14.33)),
-        CLIMB(0.0, Rotation2d.fromDegrees(270+40)),
+        CLIMB(0.0, Rotation2d.fromDegrees(270+50)),
         ALGAE_HIGH(0.445, Rotation2d.fromDegrees(60)),
         ALGAE_LOW(0.05, Rotation2d.fromDegrees(60)),
         BARGE(Meters.of(1.24).in(Meters), Rotation2d.fromDegrees(safeAlgaeArmAngle.in(Degrees))),
